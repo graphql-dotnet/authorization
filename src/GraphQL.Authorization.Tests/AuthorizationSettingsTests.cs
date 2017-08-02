b97decc93bfbce57e6a024873d31e65ebbba2830
Joe McBride <joe@uicraftsman.com>
2017-08-02 15:01:12 -0700

    Update Claims reference

---
+++ src/GraphQL.Authorization.Tests/AuthorizationSettingsTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace GraphQL.Authorization.Tests
+{
+    public class AuthorizationSettingsTests
+    {
+        private readonly AuthorizationSettings _settings;
+
+        public AuthorizationSettingsTests()
+        {
+            _settings = new AuthorizationSettings();
+        }
+
+        [Fact]
+        public void can_add_a_claim_policy()
+        {
+            _settings.AddPolicy("MyPolicy", _ =>
+            {
+                _.RequireClaim("Admin");
+            });
+
+            _settings.Policies.Count().ShouldBe(1);
+
+            var policy = _settings.Policies.Single();
+            policy.Requirements.Single().ShouldBeOfType<ClaimAuthorizationRequirement>();
+        }
+    }
+}
