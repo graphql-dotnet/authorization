2d6539a51f774135ff59de90561220cfff75b439
Joe McBride <joe@uicraftsman.com>
2020-09-02 12:56:18 -0700

    Use official 3.0 release

--- tools/tasks/test.dotnet.js
+++ tools/tasks/test.dotnet.js
@@ -6,15 +6,15 @@ export default function testDotnet() {
   const deferred = new Deferred();
 
   const platform = process.platform === 'darwin'
-    ? '-f netcoreapp2.2'
+    ? '-f netcoreapp3.1'
     : '';
   const test = `dotnet test ${platform} -c ${settings.target}`;
 
   pushd('src/GraphQL.Authorization.Tests')
   console.log(test);
 
-  exec(test, {async:true}, (code, stdout, stderr)=> {
-    if(code === 0) {
+  exec(test, { async: true }, (code, stdout, stderr) => {
+    if (code === 0) {
       deferred.resolve();
     } else {
       deferred.reject(stderr);
