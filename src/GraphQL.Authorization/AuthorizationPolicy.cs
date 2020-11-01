9c730f9ec1f662ca8c2746c77ff84faf9c3f3dd6
Ivan Maximov <sungam3r@yandex.ru>
2020-11-01 23:46:10 +0300

    xmldocs + small optimize

--- src/GraphQL.Authorization/AuthorizationPolicy.cs
+++ src/GraphQL.Authorization/AuthorizationPolicy.cs
@@ -1,22 +1,52 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GraphQL.Authorization
 {
-    public interface IAuthorizationPolicy
-    {
-        IEnumerable<IAuthorizationRequirement> Requirements { get; }
-    }
-
+    /// <summary>
+    /// Default implementation for <see cref="IAuthorizationPolicy"/>.
+    /// </summary>
     public class AuthorizationPolicy : IAuthorizationPolicy
     {
+        // allocation optimization for single requirement in policy
+        private readonly IAuthorizationRequirement _singleRequirement;
         private readonly List<IAuthorizationRequirement> _requirements;
 
+        /// <summary>
+        /// Creates a policy with a set of specified requirements.
+        /// </summary>
+        /// <param name="requirements">Specified requirements.</param>
         public AuthorizationPolicy(IEnumerable<IAuthorizationRequirement> requirements)
         {
-            _requirements = new List<IAuthorizationRequirement>(
-                requirements ?? new List<IAuthorizationRequirement>());
+            if (requirements != null)
+            {
+                var temp = requirements.ToList();
+
+                temp.Apply(req => { if (req == null) throw new ArgumentNullException(nameof(requirements), "One of the requirements is null"); });
+
+                if (temp.Count > 1)
+                    _requirements = temp;
+                else if (temp.Count == 1)
+                    _singleRequirement = temp[0];
+            }
         }
 
-        public IEnumerable<IAuthorizationRequirement> Requirements => _requirements;
+        /// <inheritdoc />
+        public IEnumerable<IAuthorizationRequirement> Requirements
+        {
+            get
+            {
+                if (_requirements != null)
+                {
+                    foreach (var r in _requirements)
+                        yield return r;
+                }
+                else if (_singleRequirement != null)
+                {
+                    yield return _singleRequirement;
+                }
+            }
+        }
     }
 }
