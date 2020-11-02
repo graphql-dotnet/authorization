ab34e958db29053022691c1f783158cac7e16881
Ivan Maximov <sungam3r@yandex.ru>
2020-11-02 22:34:39 +0300

    revert change in harness

--- src/Harness/GraphQL.cs
+++ src/Harness/GraphQL.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using GraphQL.Authorization;
 
 namespace Harness
 {
+    public class GraphQLUserContext : Dictionary<string, object>, IProvideClaimsPrincipal
+    {
+        public ClaimsPrincipal User { get; set; }
+    }
+
     public class Query
     {
         [GraphQLAuthorize(Policy = "AdminPolicy")]
