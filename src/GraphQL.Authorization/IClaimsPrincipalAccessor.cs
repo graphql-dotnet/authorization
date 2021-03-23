d0224d80717a2b5aaa681f8b36acbd7b4841403c
Ivan Maximov <sungam3r@yandex.ru>
2021-03-23 14:02:11 +0300

    1

--- src/GraphQL.Authorization/IClaimsPrincipalAccessor.cs
+++ src/GraphQL.Authorization/IClaimsPrincipalAccessor.cs
@@ -4,14 +4,14 @@ using GraphQL.Validation;
 namespace GraphQL.Authorization
 {
     /// <summary>
-    /// Provides access to the <see cref="ClaimsPrincipal"/> used for GraphQL operation authorization.
+    /// Provides access to the <see cref="ClaimsPrincipal"/> used when authorizing a GraphQL operation.
     /// </summary>
     public interface IClaimsPrincipalAccessor
     {
         /// <summary>
-        /// Provides the <see cref="ClaimsPrincipal"/> for the current <see cref="ValidationContext"/>
+        /// Provides the <see cref="ClaimsPrincipal"/> for the current <see cref="ValidationContext"/>.
         /// </summary>
-        /// <param name="context">The <see cref="ValidationContext"/> of the current operation</param>
+        /// <param name="context">The <see cref="ValidationContext"/> of the current operation.</param>
         /// <returns></returns>
         ClaimsPrincipal GetClaimsPrincipal(ValidationContext context);
     }
