c7745639dd37fc3efb933b19564bbbeb557a0863
Shane32 <shane@acdmail.com>
2021-12-12 17:46:34 -0500

    Update

--- src/GraphQL.Authorization/GraphQLBuilderExtensions.cs
+++ src/GraphQL.Authorization/GraphQLBuilderExtensions.cs
@@ -9,8 +9,8 @@ namespace GraphQL.Authorization
         /// <summary>
         /// Registers <see cref="AuthorizationEvaluator"/> and <see cref="AuthorizationValidationRule"/> within the
         /// dependency injection framework and configures the validation rule to be added to the list of validation rules
-        /// within <see cref="ExecutionOptions.ValidationRules"/> upon document execution.
-        /// Configures authorization settings with the specified configuration delegate.
+        /// within <see cref="ExecutionOptions.ValidationRules"/> and <see cref="ExecutionOptions.CachedDocumentValidationRules"/>
+        /// upon document execution. Configures authorization settings with the specified configuration delegate.
         /// </summary>
         public static void AddAuthorization(this IGraphQLBuilder builder, Action<AuthorizationSettings, IServiceProvider> configure)
         {
